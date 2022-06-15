64547693882b4d89c55fb1d7cffe31bdd79a06bb
MUHAMMAD HAZIM BIN HAFIZALSHAH <b032110058@student.utem.edu.my>
2022-06-16 07:55:00 +0800

    Piece

--- Source.cpp
+++ Source.cpp
@@ -37,8 +37,6 @@ class Engine {
     sf::VideoMode video;
     sf::Clock clock;
 
-    float deltaTime;
-
     Engine();
 
 public:
@@ -46,10 +44,13 @@ public:
     sf::View view;
     sf::Event event;
 
+    float deltaTime;
+
     Engine(Engine& other) = delete;
     void operator=(const Engine&) = delete;
     static Engine& instance();
     void render(sf::Sprite& sprite);
+    void render(sf::Text& text);
     sf::Event& next();
     sf::Vector2f getMousePosition();
 };
@@ -61,9 +62,10 @@ Engine::Engine():
     view(sf::FloatRect(0, 0, 1920, 1080)),
     deltaTime(0.f)
 {
-    window.setView(view);
     srand(time(NULL));
+    window.setView(view);
     window.setMouseCursorVisible(false);
+    window.setKeyRepeatEnabled(false);
 }
 
 Engine& Engine::instance() {
@@ -74,6 +76,11 @@ Engine& Engine::instance() {
 void Engine::render(sf::Sprite& sprite)
 {
     window.draw(sprite);
+}
+
+void Engine::render(sf::Text& text)
+{
+    window.draw(text);
 }
 
 sf::Event& Engine::next()
@@ -97,6 +104,7 @@ protected:
     Engine& engine;
 
 public:
+    virtual bool pass() = 0;
     virtual void execute() = 0;
 };
 
@@ -111,15 +119,11 @@ protected:
 
     ActorSprite(std::string fp);
     ActorSprite(std::string fp, int x, int y, int w, int h);
+    ActorSprite(std::string fp, sf::IntRect ir);
     virtual ~ActorSprite();
     void draw();
+    virtual bool pass() override;
 };
-
-void ActorSprite::draw() {
-    engine.render(sprite);
-}
-
-ActorSprite::~ActorSprite() {}
 
 ActorSprite::ActorSprite(std::string fp) {
     if (texture.loadFromFile(fp))
@@ -135,6 +139,61 @@ ActorSprite::ActorSprite(std::string fp, int x, int y, int w, int h) {
     }
 }
 
+ActorSprite::ActorSprite(std::string fp, sf::IntRect ir) {
+    if (texture.loadFromFile(fp, ir))
+    {
+        sprite.setTexture(texture);
+    }
+}
+
+ActorSprite::~ActorSprite() {}
+
+void ActorSprite::draw() {
+    engine.render(sprite);
+}
+
+bool ActorSprite::pass() {
+    return true;
+}
+
+class ActorText : public Actor {
+protected:
+    sf::Font font;
+    sf::Text text;
+
+    ActorText(std::string fp);
+    ActorText(std::string fp, std::string s);
+    virtual ~ActorText();
+    void draw();
+    virtual bool pass() override;
+};
+
+void ActorText::draw() {
+    engine.render(text);
+}
+
+ActorText::~ActorText() {}
+
+ActorText::ActorText(std::string fp) {
+    if (font.loadFromFile(fp))
+    {
+        text.setFont(font);
+    }
+}
+
+ActorText::ActorText(std::string fp, std::string s) {
+    if (font.loadFromFile(fp))
+    {
+        text.setFont(font);
+    }
+
+    text.setString("Hello world");
+}
+
+bool ActorText::pass() {
+    return true;
+}
+
 class Cursor : public ActorSprite {
     ~Cursor();
 public:
@@ -142,6 +201,7 @@ public:
 
     Cursor();
     void execute() override;
+    virtual bool pass() override;
 };
 
 Cursor::Cursor() : ActorSprite("./Assets/GUI/Pick.png") {
@@ -150,23 +210,75 @@ Cursor::Cursor() : ActorSprite("./Assets/GUI/Pick.png") {
 Cursor::~Cursor() {}
 
 void Cursor::execute() {
-    cursorHitbox = sprite.getGlobalBounds();
     sprite.setPosition(engine.getMousePosition());
     ActorSprite::draw();
 }
 
+bool Cursor::pass() {
+    return false;
+}
+
+class Piece : public ActorSprite {
+    ~Piece();
+public:
+    enum PIECE{
+        WhitePawn,
+        WhiteKnight,
+        WhiteBishop,
+        WhiteRook,
+        WhiteQueen,
+        WhiteKing,
+        BlackPawn,
+        BlackKnight,
+        BlackBishop,
+        BlackRook,
+        BlackQueen,
+        BlackKing
+    };
+
+    sf::IntRect pieceRect(int i);
+    Piece(int i);
+    void execute() override;
+    virtual bool pass() override;
+};
+
+sf::IntRect Piece::pieceRect(int i) {
+    const int TILESIZE = 128;
+    int x = 0, y = 0;
+
+    x = i;
+
+    if (i > 5) {
+        x =- 5;
+        y = TILESIZE;
+    }
+
+    return sf::IntRect(x, y, TILESIZE, TILESIZE);
+}
+
+Piece::Piece(int i) : ActorSprite("./Assets/Sprites/Pieces.png", pieceRect(i)) {}
+
+Piece::~Piece() {}
+
+void Piece::execute() {
+    ActorSprite::draw();
+}
+
+bool Piece::pass() {
+    return true;
+}
+
 class Board : public ActorSprite {
-    Cursor& cursor;
     int selection[2];
     ~Board();
 public:
-    Board(Cursor& c);
+    Board();
     void addSelection();
     void execute() override;
+    virtual bool pass() override;
 };
 
-Board::Board(Cursor& c) : ActorSprite("./Assets/Sprites/Board.png"),
-cursor(c),
+Board::Board() : ActorSprite("./Assets/Sprites/Board.png"),
 selection{ 0, 0 } {
     sprite.setPosition(engine.window.getSize().x / 2 - sprite.getLocalBounds().width / 2,
         engine.window.getSize().y / 2 - sprite.getLocalBounds().height / 2);
@@ -175,46 +287,120 @@ selection{ 0, 0 } {
 Board::~Board() {}
 
 void Board::addSelection() {
-    sf::FloatRect intersection;
+    static int index = 0;
 
-    sprite.getGlobalBounds().intersects(cursor.cursorHitbox, intersection);
+    if (index > 1)
+        index = 0;
 
-    for (int i = 0; i < 8; i++) {
-        if ((i + 1) * 128 > intersection.left)
+    for (int i = 1; i < 8; i++) {
+        if ((i) * 128  + engine.window.getSize().x / 2 - sprite.getLocalBounds().width / 2 > engine.getMousePosition().x)
             break;
-        selection[0] = i;
+        selection[index] = i * 100;
     }
 
-    for (int i = 0; i < 8; i++) {
-        if ((i + 1) * 128 > intersection.top)
+    for (int i = 1; i < 8; i++) {
+        if ((i) * 128 + engine.window.getSize().y / 2 - sprite.getLocalBounds().height / 2 > engine.getMousePosition().y)
             break;
-        selection[1] = i;
+        selection[index]++;
     }
 
-    printf("%f, %f\n", intersection.left, intersection.top);
+    selection[index] += 101;
+
+    index++;
 }
 
 void Board::execute() {
-    if (engine.event.type == sf::Event::MouseButtonReleased && sprite.getGlobalBounds().contains(engine.getMousePosition()))
+    static float timer = 0.1f;
+
+    if (timer > 0.1f && engine.event.type == sf::Event::MouseButtonPressed && sprite.getGlobalBounds().contains(engine.getMousePosition())) {
         addSelection();
+
+        timer = 0.f;
+    }
+    else
+        timer += engine.deltaTime;
+    
     ActorSprite::draw();
 }
 
-short int level[64];
+bool Board::pass() {
+    return true;
+}
 
-int main()
-{
+int arr[64] = {
+    -4, -2, -3, -5, -6, -3, -2, -4,
+    -1, -1, -1, -1, -1, -1, -1, -1,
+     0,  0,  0,  0,  0,  0,  0,  0,
+     0,  0,  0,  0,  0,  0,  0,  0,
+     0,  0,  0,  0,  0,  0,  0,  0,
+     0,  0,  0,  0,  0,  0,  0,  0,
+     1,  1,  1,  1,  1,  1,  1,  1,
+     4,  2,  3,  5,  6,  3,  2,  4
+};
+
+class Game {
+    Engine& engine;
     std::vector<Actor*> actors;
 
+    int kept;
+    int timer;
+    bool countdown;
+
+public:
+    Game();
+    ~Game();
+    
+    void clearActors();
+    void play();
+};
+
+Game::Game():
+    engine(Engine::instance()),
+    kept(0),
+    timer(100),
+    countdown(false) {
+
     Cursor* c = new Cursor();
-    Board* as = new Board(*c);
-    //Board* b = new Board("./Assets/Sprites/Pieces.png", 3 * 128, 0, 128, 128);
-    actors.push_back(as);
-    actors.push_back(c);
-    //actors.push_back(b);
+    Board* as = new Board();
+    Piece* b = new Piece(Piece::PIECE::WhitePawn);
+    
+    actors.insert(actors.end() - kept, as);
+    actors.insert(actors.end() - kept, b);
+    actors.insert(actors.end() - kept, c);
+
+}
+
+Game::~Game() {
+    actors.clear();
+}
+
+void Game::clearActors() {
+    kept = 0;
+
+    for (Actor* a : actors) {
+        if (a->pass()) {
+            actors.erase(actors.begin() + kept);
+            kept--;
+        }
+        kept++;
+        printf("%i", kept);
+    }
+}
+
+void Game::play() {
 
     while (Engine::instance().window.isOpen()) {
-        sf::Event event = Engine::instance().next();
+        sf::Event& event = Engine::instance().next();
+
+        if(countdown)
+            timer -= engine.deltaTime;
+
+        if (timer <= 0) {
+            clearActors();
+            timer = 100;
+        }
+
+        kept = 0;
 
         switch (event.type)
         {
@@ -230,6 +416,22 @@ int main()
 
         Engine::instance().window.display();
     }
-    
-    actors.clear();
 }
+
+int main()
+{
+    Game g;
+    g.play();
+}
+
+
+
+//kept = 0;
+//
+//for (Actor* a : actors) {
+//  if (a->pass()) {
+//      actors.erase(actors.begin() + kept);
+//      kept--;
+//  }
+//kept++;
+//printf("%i", kept);
