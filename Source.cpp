7c4e328c9074055e177b4d61e12680de2c48f603
MUHAMMAD HAZIM BIN HAFIZALSHAH <b032110058@student.utem.edu.my>
2022-06-15 21:10:12 +0800

    Added Cursor and Board classes.

--- Source.cpp
+++ Source.cpp
@@ -1,6 +1,35 @@
 #include <vector>
 #include <SFML/Graphics.hpp>
 
+namespace Utilities {
+    sf::Vector2f linearInterpolate(sf::Vector2f a, sf::Vector2f b, float t) {
+        return sf::Vector2f(a + sf::Vector2f(t * (b - a).x, t * (b - a).y));
+    }
+
+    sf::Vector2f Normalize(sf::Vector2f v) {
+        float l = std::sqrt(std::pow(v.x, 2) + std::pow(v.y, 2));
+
+        return l == 0 ? sf::Vector2f() : sf::Vector2f(
+            v.x /
+            l,
+            v.y /
+            l);
+    }
+
+    float distanceBetween(sf::Vector2f a, sf::Vector2f b) {
+        return sqrt(pow(b.x - a.x, 2) + pow(b.y - a.y, 2) * 1.f);
+    }
+
+    float radiansToDegrees(float f) {
+        return f * (180.0f / 3.141592653589793238463f);
+    }
+
+    float DegreesToRadians(float f) {
+        return f * (3.141592653589793238463f / 180.0f);
+    }
+
+}
+
 //Engine is a singleton
 class Engine {
     static Engine* engine;
@@ -16,22 +45,25 @@ public:
     sf::RenderWindow window;
     sf::View view;
     sf::Event event;
+
     Engine(Engine& other) = delete;
     void operator=(const Engine&) = delete;
     static Engine& instance();
-
     void render(sf::Sprite& sprite);
     sf::Event& next();
+    sf::Vector2f getMousePosition();
 };
 
 Engine::Engine():
     video(1920, 1080),
     window(this->video, "Game"),
+    //window(this->video, "Game", sf::Style::Fullscreen),
     view(sf::FloatRect(0, 0, 1920, 1080)),
     deltaTime(0.f)
 {
     window.setView(view);
     srand(time(NULL));
+    window.setMouseCursorVisible(false);
 }
 
 Engine& Engine::instance() {
@@ -51,6 +83,10 @@ sf::Event& Engine::next()
     window.clear();
     
     return event;
+}
+
+sf::Vector2f Engine::getMousePosition() {
+    return window.mapPixelToCoords(sf::Mouse::getPosition(window));
 }
 
 class Actor {
@@ -99,18 +135,47 @@ ActorSprite::ActorSprite(std::string fp, int x, int y, int w, int h) {
     }
 }
 
-class Board : public ActorSprite {
-    ~Board();
+class Cursor : public ActorSprite {
+    ~Cursor();
 public:
-    Board();
+    Cursor();
     void execute() override;
 };
 
-Board::Board() : ActorSprite("./Assets/Sprites/BlackTile.png") {}
+Cursor::Cursor() : ActorSprite("./Assets/GUI/Pick.png") {
+}
+
+Cursor::~Cursor() {}
+
+void Cursor::execute() {
+    sprite.setPosition(engine.getMousePosition());
+
+    ActorSprite::draw();
+}
+
+class Board : public ActorSprite {
+    int selection[2];
+    ~Board();
+public:
+    Board();
+    void addSelection(sf::Vector2f p);
+    void execute() override;
+};
+
+Board::Board() : ActorSprite("./Assets/Sprites/Board.png"),
+selection{ 0, 0 } {
+    sprite.setPosition(engine.window.getSize().x / 2 - sprite.getLocalBounds().width / 2,
+        engine.window.getSize().y / 2 - sprite.getLocalBounds().height / 2);
+}
 
 Board::~Board() {}
 
+void Board::addSelection(sf::Vector2f p) {
+    printf("%f, %f\n", p.x, p.y);
+}
+
 void Board::execute() {
+
     ActorSprite::draw();
 }
 
@@ -118,21 +183,26 @@ short int level[64];
 
 int main()
 {
-    Engine::instance;
     std::vector<Actor*> actors;
 
-    //Board board[64];
-
     Board* as = new Board();
+    Cursor* c = new Cursor();
     //Board* b = new Board("./Assets/Sprites/Pieces.png", 3 * 128, 0, 128, 128);
     actors.push_back(as);
+    actors.push_back(c);
     //actors.push_back(b);
 
     while (Engine::instance().window.isOpen()) {
         sf::Event event = Engine::instance().next();
 
-        if (event.type == sf::Event::Closed)
+        switch (event.type)
+        {
+        case sf::Event::Closed:
             Engine::instance().window.close();
+            break;
+        default:
+            break;
+        }
 
         for (Actor* a : actors)
             a->execute();
